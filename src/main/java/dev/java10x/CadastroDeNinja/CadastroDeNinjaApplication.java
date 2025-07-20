72949a63e4082faf2e952836c26709568a2c8cd8
Gabriel976846 <sergiogabriel_santos18@hotmail.com>
2025-07-20 14:25:50 -0300

    Criado a controller

--- src/main/java/dev/java10x/CadastroDeNinja/CadastroDeNinjaApplication.java
+++ src/main/java/dev/java10x/CadastroDeNinja/CadastroDeNinjaApplication.java
@@ -1,5 +1,4 @@
 package dev.java10x.CadastroDeNinja;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/dev/java10x/CadastroDeNinja/Controller.java
@@ -0,0 +1,19 @@
+package dev.java10x.CadastroDeNinja;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("boasvindas")
+public class Controller {
+
+    @GetMapping
+    public String boasVindas(){
+        return "Bem vindo a aplicação spring";
+    }
+
+    @GetMapping("/tchau")
+    public String despedida(){
+        return "Até mais  ";
+    }
+}
