9edee52e9d168e35b1df4eb91337b7ba51073bd5
Gabriel976846 <sergiogabriel_santos18@hotmail.com>
2025-10-12 14:07:01 -0300

    feat: Coloca o lombok nas classes para abstração

--- src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesModel.java
@@ -1,43 +1,26 @@
 package dev.java10x.CadastroDeNinja.Missoes;
+
 import dev.java10x.CadastroDeNinja.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_missao")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class MissoesModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
-    private char rankmissao;
-    @OneToMany(mappedBy = "missoes") //
-    private NinjaModel ninja;
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(long id, String nome, char rankmissao) {
-        this.id = id;
-        this.nome = nome;
-        this.rankmissao = rankmissao;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public char getRankmissao() {
-        return rankmissao;
-    }
-
-    public void setRankmissao(char rankmissao) {
-        this.rankmissao = rankmissao;
-    }
-
+    private char rank;
+    @OneToMany(mappedBy = "missoes") // RELAÇÃO 1:N -- UMA MISSÃO PODE TER VARIOS NINJAS
+    private List<NinjaModel> ninjas;
 
 }
--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaModel.java
@@ -2,52 +2,24 @@ package dev.java10x.CadastroDeNinja.Ninjas;
 
 import dev.java10x.CadastroDeNinja.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity // TRANFORMAR A CLASSE EM UMA ENTIDADE DO BD
-@Table(name = "tb_ninjas") // DAR NOME DA TABELA
+@Table(name = "tb_cadastro") // NOME DA TABELA
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class NinjaModel {
     @Id // GERA O ID
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // GERA O ID DE FORMA CRESCENTE
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // GERA O ID DE FORMA CRESCENTE E AUTOMATICAMENTE
     private long id;
     private String nome;
     private String email;
     private int idade;
-    @ManyToOne // VARIOS PARA 1
-    @JoinColumn(name = "missoes_id") // CHAVE ESTRANGEIRA
-    private List<MissoesModel> missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
+    @ManyToOne // RELAÇÃO N:1 -- UM NINJA TEM UMA UNICA MISSÃO
+    @JoinColumn(name = "missoes_id") // GERA A CHAVE ESTRAGENGEIRA FK
+    private MissoesModel missoes;
 }
